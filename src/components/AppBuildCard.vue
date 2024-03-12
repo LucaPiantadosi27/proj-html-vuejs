6f582707643d24664f4f66eda4f7e509a8a519c0
Luca Piantadosi <lu.piantadosi125@gmail.com>
2024-03-12 11:51:41 +0100

    Events added

--- src/components/AppBuildCard.vue
+++ src/components/AppBuildCard.vue
@@ -16,7 +16,7 @@ export default {
     <div class="card text-center border-0 h-100">
         <img :src="getImgUrl(img)" class="px-4 card-img-top h-50 object-fit-scale" :alt="title">
         <div class="card-body">
-            <h5 class="card-title fw-semibold">{{ title }}</h5>
+            <h5 class="card-title fw-bold mt-3">{{ title }}</h5>
             <p class="card-text text-light-emphasis">{{ text }}</p>
             <a href="#" class="card-link ms_color_violet link-underline link-underline-opacity-0 fw-semibold ms_hover">Start
                 now
--- src/components/AppMainTestimonials.vue
+++ src/components/AppMainTestimonials.vue
@@ -1,15 +1,20 @@
 <script>
+import { store } from "../store.js";
+import AppTitle from "./AppTitle.vue";
 export default {
     data() {
         return {
-        }
-    }
+            store,
+        };
+    },
+    components: { AppTitle }
 }
 </script>
 
 <template>
-    <div class="wrapper d-flex">
-        <div class="opinions_card">opinions</div>
+    <div class="wrapper">
+        <AppTitle :Mono="store.titleSections[5].titleMono" :Purple="store.titleSections[5].titlePurple"
+            :Green="store.titleSections[5].titleGreen" :class_align="'text-center'" />
     </div>
 </template>
 
